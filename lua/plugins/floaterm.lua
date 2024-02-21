7cfc89ff2b348ccc29e84f3b16162f204e5b8a5d
Shkalin Kirill <kirillshkalin@gmail.com>
2024-02-21 14:58:04 +0300

    Add verbatim snippet. Add cmake keymaps. Add spellchecking

--- lua/plugins/floaterm.lua
+++ lua/plugins/floaterm.lua
@@ -13,7 +13,7 @@ function C.SetCMakeMaps(cmake_args)
     gen_str = 'cmake -S . -B build ' .. cmake_args
     build_str = 'cmake --build build'
     test_str = 'ctest --test-dir build/tests'
-    vtest_str = test_str .. '-V --rerun-failed --output-on-failure'
+    vtest_str = test_str .. ' -V --rerun-failed --output-on-failure'
     vim.keymap.set('n',
                    '<leader>cg',
                    ':FloatermNew --autoclose=1 ' .. gen_str .. '<CR>')
@@ -22,13 +22,13 @@ function C.SetCMakeMaps(cmake_args)
                    ':FloatermNew --autoclose=1 ' .. build_str .. '<CR>')
     vim.keymap.set('n',
                    '<leader>ct',
-                   ':FloatermNew --autoclose=1 ' .. test_str .. '<CR>')
+                   ':FloatermNew --autoclose=0 ' .. test_str .. '<CR>')
     vim.keymap.set('n',
                    '<leader>cv',
-                   ':FloatermNew --autoclose=1 ' .. vtest_str .. '<CR>')
+                   ':FloatermNew --autoclose=0 ' .. vtest_str .. '<CR>')
     vim.keymap.set('n',
                    '<leader>cc',
-                   ':FloatermNew! --autoclose=1 ' .. gen_str .. ' && '
+                   ':FloatermNew! --autoclose=0 ' .. gen_str .. ' && '
                     .. build_str .. ' && '
                     .. test_str .. '<CR>')
 end
