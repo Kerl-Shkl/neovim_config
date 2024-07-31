fdb12be92ad6cb3551548926f4b36553ab53349f
k.shkalin <k.shkalin@yadro.com>
2024-07-31 18:18:36 +0300

    Add "+y/p on <leader-y/p>

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -3,3 +3,8 @@ vim.g.mapleader = " "
 -- Neotree
 vim.keymap.set('n', '<leader>n', ':Neotree float toggle focus<CR>')
 vim.keymap.set('n', '<C-n>', ':Neotree left toggle focus<CR>')
+
+-- space y to copy to clipboard
+vim.keymap.set({"n", "v", "o"}, "<leader>y", "\"+y", { remap = true })
+-- space p to past to clipboard
+vim.keymap.set({"n"}, "<leader>p", "\"+p", { remap = true })
