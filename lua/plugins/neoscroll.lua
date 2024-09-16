9bad1ef19ad907abfabddeced9a99968985ad0d2
k.shkalin <k.shkalin@yadro.com>
2024-09-16 10:48:53 +0300

    Fix neoscroll config after lazy update

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -1,10 +1,18 @@
-require('neoscroll').setup{
+neoscroll = require('neoscroll')
+
+neoscroll.setup{
     mappings = {'<C-u>', '<C-d>', 'zt', 'zz', 'zb'},
-    hide_cursor = false;
+    hide_cursor = false,
 }
 
-local t = {}
-t['<C-u>'] = {'scroll', {'-vim.wo.scroll', 'false', '150'}}
-t['<C-d>'] = {'scroll', {'vim.wo.scroll', 'false', '150'}}
-
-require('neoscroll.config').set_mappings(t)
+local t = {
+    ["<C-u>"] = function() neoscroll.ctrl_u({move_cursor=false; duration = 150}) end;
+    ["<C-d>"] = function() neoscroll.ctrl_d({move_cursor=false; duration = 150}) end;
+    ["zt"]    = function() neoscroll.zt({ half_win_duration = 150 }) end;
+    ["zz"]    = function() neoscroll.zz({ half_win_duration = 150 }) end;
+    ["zb"]    = function() neoscroll.zb({ half_win_duration = 150 }) end;
+}
+local modes = { 'n', 'v', 'x' }
+for key, func in pairs(t) do
+    vim.keymap.set(modes, key, func)
+end
