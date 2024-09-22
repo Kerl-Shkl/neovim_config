00b5545a4a7e192a975efc8a7ba1d201780ce6a0
Kerl-Shkl <kirillshkalin@gmail.com>
2024-09-22 10:40:08 +0300

    Divide git signs and diagnostic signs in statuscol

--- lua/plugins/statuscol.lua
+++ lua/plugins/statuscol.lua
@@ -22,6 +22,10 @@ require("statuscol").setup({
             condition = { true, builtin.not_empty },
             click = "v:lua.ScLa",
         },
+        {
+            sign = { namespace = {'gitsign'}, maxwidth = 1, auto = true },
+            click = 'v:lua.ScSa'
+        },
         -- { text = { "%s" }, click = "v:lua.ScSa" },
     },
     clickmod = "c",         -- modifier used for certain actions in the builtin clickhandlers:
@@ -38,42 +42,3 @@ require("statuscol").setup({
         gitsigns                = builtin.gitsigns_click,
     },
 })
-
--- function statuscolSetup()
---     local builtin = require("statuscol.builtin")
---     require("statuscol").setup({
---         setopt = true,         -- Whether to set the 'statuscolumn' option, may be set to false for those who
---         -- want to use the click handlers in their own 'statuscolumn': _G.Sc[SFL]a().
---         -- Although I recommend just using the segments field below to build your
---         -- statuscolumn to benefit from the performance optimizations in this plugin.
---         -- builtin.lnumfunc number string options
---         thousands = false,     -- or line number thousands separator string ("." / ",")
---         relculright = true,   -- whether to right-align the cursor line number with 'relativenumber' set
---         -- Builtin 'statuscolumn' options
---         ft_ignore = nil,       -- lua table with 'filetype' values for which 'statuscolumn' will be unset
---         bt_ignore = nil,       -- lua table with 'buftype' values for which 'statuscolumn' will be unset
---         -- Default segments (fold -> sign -> line number + separator), explained below
---         segments = {
---             { text = { "%C" }, click = "v:lua.ScFa" },
---             { text = { "%s" }, click = "v:lua.ScSa" },
---             {
---                 text = { builtin.lnumfunc, " " },
---                 condition = { true, builtin.not_empty },
---                 click = "v:lua.ScLa",
---             }
---         },
---         clickmod = "c",         -- modifier used for certain actions in the builtin clickhandlers:
---         -- "a" for Alt, "c" for Ctrl and "m" for Meta.
---         clickhandlers = {       -- builtin click handlers
---             Lnum                    = builtin.lnum_click,
---             FoldClose               = builtin.foldclose_click,
---             FoldOpen                = builtin.foldopen_click,
---             FoldOther               = builtin.foldother_click,
---             DapBreakpointRejected   = builtin.toggle_breakpoint,
---             DapBreakpoint           = builtin.toggle_breakpoint,
---             DapBreakpointCondition  = builtin.toggle_breakpoint,
---             ["diagnostic/signs"]    = builtin.diagnostic_click,
---             gitsigns                = builtin.gitsigns_click,
---         },
---     })
--- end
