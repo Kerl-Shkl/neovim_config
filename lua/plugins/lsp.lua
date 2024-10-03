afc14c144856bd6b6cee8c17f1d3893a8d172dc1
k.shkalin <k.shkalin@yadro.com>
2024-10-03 16:32:34 +0300

    Add border for float diagnostic window and lsp hover

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -59,3 +59,21 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
 vim.keymap.set('n', '<F4>', ':ClangdSwitchSourceHeader<CR>')
 vim.diagnostic.config({ virtual_text = false, virtual_improved = { current_line ='only', }, })
+
+local _border = "single"
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover, {
+    border = _border
+  }
+)
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+  vim.lsp.handlers.signature_help, {
+    border = _border
+  }
+)
+
+vim.diagnostic.config{
+  float={border=_border}
+}
