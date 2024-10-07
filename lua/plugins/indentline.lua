3290d9443349737c652e68a6a5fe8b1be1506b83
k.shkalin <k.shkalin@yadro.com>
2024-10-07 19:04:43 +0300

    Not bad

---
+++ lua/plugins/indentline.lua
@@ -0,0 +1,13 @@
+require("ibl").setup(
+    {
+        indent = {
+            char = "▏",
+            tab_char = { "a", "b", "c" }
+        },
+        scope = {
+            show_start=false,
+            show_end=false,
+        }
+
+    }
+)
--- lua/plugins/statuscol.lua
+++ lua/plugins/statuscol.lua
@@ -20,7 +20,7 @@ require("statuscol").setup({
         --     sign = { namespace = {'gitsign'}, maxwidth = 1, auto = true },
         --     click = 'v:lua.ScSa'
         -- },
-        { text = {builtin.foldfunc}, click = "v:lua.ScFa" },
+        -- { text = {builtin.foldfunc}, click = "v:lua.ScFa" },
         {
             text = { builtin.lnumfunc, " " },
             condition = { true, builtin.not_empty },
