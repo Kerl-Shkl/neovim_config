02178d4d36df345d82f183342e292f304fee617b
Kerl-Shkl <kirillshkalin@gmail.com>
2025-06-29 18:51:13 +0300

    Add latex support with vimtex and snippets

--- lua/snippets/tex.lua
+++
@@ -1,6 +0,0 @@
-require("luasnip.loaders.from_lua").lazy_load()
-local utils = require "snippets.utils"
-
-return {
-    s("ff", fmta("\\frac{<>}{<>}", { i(1), i(2) }), { condition = utils.in_mathzone }),
-}
