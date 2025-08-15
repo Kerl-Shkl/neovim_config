2ae7525304d54f71a27dda76ccf3cf629525a642
k.shkalin <k.shkalin@yadro.com>
2025-08-15 16:45:28 +0300

    Add queries for better dataservices programming experience

--- lua/core/colors.lua
+++ lua/core/colors.lua
@@ -23,6 +23,7 @@ function sensibleHighlightning()
     vim.api.nvim_set_hl(0, "@variable", { fg = colors.foreground })
     vim.api.nvim_set_hl(0, "@variable.member", { link = "@variable" })
     vim.api.nvim_set_hl(0, "@variable.parameter", { link = "@variable" })
+    vim.api.nvim_set_hl(0, "@variable.builtin", { link = "@variable" })
     vim.api.nvim_set_hl(0, "@property", { link = "@variable.member" })
 
     vim.api.nvim_set_hl(0, "@function", { fg = colors.foreground })
@@ -39,23 +40,33 @@ function sensibleHighlightning()
     vim.api.nvim_set_hl(0, "@keyword.class.modifier", { link = "@keyword" })
     vim.api.nvim_set_hl(0, "@keyword.directive", { link = "@keyword" })
     vim.api.nvim_set_hl(0, "@attribute.cpp", { link = "@keyword" })
-    vim.api.nvim_set_hl(0, "@module", { link = "@keyword" })
+
+    -- vim.api.nvim_set_hl(0, "@module", { link = "@keyword" })
+    vim.api.nvim_set_hl(0, "@module", { fg = colors.medium_gray })
+    vim.api.nvim_set_hl(0, "@namespace_operator", { link = "@module" }) -- medium_gray is interesting variant
 
     vim.api.nvim_set_hl(0, "@operator", { fg = colors.milk })
     vim.api.nvim_set_hl(0, "@keyword.operator", { link = "@operator" })
-    vim.api.nvim_set_hl(0, "@comment", { fg = colors.soft_yellow })
-    vim.api.nvim_set_hl(0, "@constant", { fg = colors.milk })
+    vim.api.nvim_set_hl(0, "@constant", { fg = colors.foreground })
     vim.api.nvim_set_hl(0, "@constant.builtin", { link = "@constant" })
+
+    vim.api.nvim_set_hl(0, "@comment", { fg = colors.soft_yellow })
+    vim.api.nvim_set_hl(0, "@comment.not_interesting", { fg = colors.comment })
 
     vim.api.nvim_set_hl(0, "@keyword.modifier", { link = "@type" })
     vim.api.nvim_set_hl(0, "@refdecl", { link = "@type" })
 
     vim.api.nvim_set_hl(0, "@markup.heading.gitcommit", { fg = colors.orange })
 
+    vim.api.nvim_set_hl(0, "@lambda_declare_punct", { sp = "#FF0000"})
+    vim.api.nvim_set_hl(0, "@folly_cont", { fg = colors.light_blue })
+    vim.api.nvim_set_hl(0, "@folly_cont_braces", { link = "@folly_cont" })
+
     vim.api.nvim_set_hl(0, "Added", { fg = "#689d6a" })
     vim.api.nvim_set_hl(0, "Removed", { fg = "#cc241d" })
     vim.api.nvim_set_hl(0, "Changed", { fg = "#eebd35" })
     vim.api.nvim_set_hl(0, "Underlined", { fg = colors.light_blue, underline = true })
+
 end
 
 function MyGruvbox()
--- queries/cpp/highlights.scm
+++ queries/cpp/highlights.scm
@@ -15,3 +15,32 @@
   "final"
   "virtual"
 ] @keyword.class.modifier
+
+(lambda_capture_specifier ["[" "]"] @lambda_declare_punct (#set! priority 105))
+(lambda_expression (compound_statement ["{" "}"] @lambda_declare_punct (#set! priority 105)))
+(lambda_declarator (parameter_list ["(" ")"] @lambda_declare_punct (#set! priority 105)))
+
+; folly queryies
+(call_expression
+    ((field_expression (field_identifier) @folly_cont (#any-of? @folly_cont
+        "then" "thenValue" "thenValueInline" "thenTry" "thenTryInline" "thenError" "thenErrorInline"
+    )))
+    (argument_list ["(" ")"] @folly_cont_braces)
+)
+
+(qualified_identifier
+  (namespace_identifier) @nt (#lua-match? @nt "^[^%u]") (#not-lua-match? @nt "_t$")
+  "::" @namespace_operator (#set! priority 105)
+)
+
+((namespace_identifier) @type
+  (#lua-match? @type "_t$"))
+
+; ("::" @namespace_operator)
+
+; Not interesting comments
+((comment) @comment.not_interesting
+  (#lua-match? @comment.not_interesting "//$"))
+
+((comment) @comment.not_interesting
+  (#lua-match? @comment.not_interesting "// ?NOLINT"))
