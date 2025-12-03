9e0e3f3e3e0c9385e65ddb3b68fd1475fa24ea17
k.shkalin <k.shkalin@yadro.com>
2025-12-04 00:07:52 +0300

    Fix ftplugins

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,4 +1,5 @@
-vim.opt.textwidth=80
+vim.opt_local.textwidth=80
 
-vim.opt.spell=true
-vim.opt.spelllang={"en_us", "ru_ru"}
+vim.opt_local.spell=true
+vim.opt_local.spelllang={"en_us", "ru_ru"}
+vim.opt_local.matchpairs:append {"`:`"}
--- ftplugin/text.lua
+++ ftplugin/text.lua
@@ -1,2 +1,1 @@
-vim.opt.textwidth = 80
-
+vim.opt_local.textwidth = 80
