03fe5bfc6f6b1d984e28754adfea01cc0924d749
k.shkalin <k.shkalin@yadro.com>
2026-01-12 23:34:23 +0300

    Small improvements

--- lua/plugins/indentline.lua
+++ lua/plugins/indentline.lua
@@ -1,13 +1,10 @@
-require("ibl").setup(
-    {
-        indent = {
-            char = "▏",
-            tab_char = { "a", "b", "c" }
-        },
-        scope = {
-            show_start=false,
-            show_end=false,
-        }
-
-    }
-)
+require("ibl").setup({
+    indent = {
+        char = "▏",
+        -- tab_char = { "a", "b", "c" }
+    },
+    scope = {
+        show_start = false,
+        show_end = false,
+    },
+})
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -56,7 +56,10 @@ vim.api.nvim_create_autocmd("LspAttach", {
         local opts = { buffer = ev.buf }
         vim.keymap.set("n", "gD", declaration_wrap, opts)
         vim.keymap.set("n", "gd", definition_wrap, opts)
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        -- vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "K", function()
+            vim.lsp.buf.hover({ border = "rounded" })
+        end, opts)
         vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
         vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
         vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
@@ -75,27 +78,31 @@ vim.api.nvim_create_autocmd("LspAttach", {
 })
 
 local function my_switch_source_header(bufnr)
-  local method_name = 'textDocument/switchSourceHeader'
-  bufnr = util.validate_bufnr(bufnr)
-  local client = vim.lsp.get_clients({ bufnr = bufnr, name = 'clangd' })[1]
-  if not client then
-    return vim.notify(('method %s is not supported by any servers active on the current buffer'):format(method_name))
-  end
-  local params = vim.lsp.util.make_text_document_params(bufnr)
-  client.request(method_name, params, function(err, result)
-    if err then
-      error(tostring(err))
+    local method_name = "textDocument/switchSourceHeader"
+    bufnr = util.validate_bufnr(bufnr)
+    local client = vim.lsp.get_clients({ bufnr = bufnr, name = "clangd" })[1]
+    if not client then
+        return vim.notify(
+            ("method %s is not supported by any servers active on the current buffer"):format(method_name)
+        )
     end
-    if not result then
-      vim.notify('corresponding file cannot be determined')
-      return
-    end
-    vim.cmd.drop(vim.uri_to_fname(result))
-  end, bufnr)
+    local params = vim.lsp.util.make_text_document_params(bufnr)
+    client.request(method_name, params, function(err, result)
+        if err then
+            error(tostring(err))
+        end
+        if not result then
+            vim.notify("corresponding file cannot be determined")
+            return
+        end
+        vim.cmd.drop(vim.uri_to_fname(result))
+    end, bufnr)
 end
 
 -- vim.keymap.set("n", "<F4>", ":ClangdSwitchSourceHeader<CR>")
-vim.keymap.set('n', "<F4>", function() my_switch_source_header(0) end)
+vim.keymap.set("n", "<F4>", function()
+    my_switch_source_header(0)
+end)
 vim.diagnostic.config({
     signs = {
         text = {
