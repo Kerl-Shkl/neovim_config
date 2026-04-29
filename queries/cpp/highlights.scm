5074af1917992fb3b20563d350c317ec5f0ebe3e
k.shkalin <k.shkalin@yadro.com>
2026-04-29 13:37:53 +0300

    Add label highlight

--- queries/cpp/highlights.scm
+++ queries/cpp/highlights.scm
@@ -37,3 +37,6 @@
 ; Not interesting comments
 ((comment) @comment.not_interesting
   (#vim-match? @comment.not_interesting "\(//$\|// ?NOLINT\|// clang-format\)"))
+
+(goto_statement "goto" @goto_op (#set! priority 105))
+(case_statement "default" @default_case (#set! priority 105))
